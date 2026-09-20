b0bbf884a45aa7ab6b213c4427777fceb7544a25
0098-sayan <maitysayan704@gmail.com>
2026-09-20 20:13:58 +0530

    complete navbar section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -6,9 +8,23 @@ export default function Home() {
       <main className="flex-1">
         {/* Hero Section */}
         <section className="container mx-auto px-4 py-32">
-          <div>
-            <h1 className="text-black">A Better way to track your job application</h1>
-            <p className="text-gray-700">Capture , Organize, and manage your job search in one place</p>
+          <div className="mx-auto max-w-4xl text-center">
+            <h1 className="text-black mb-6 text-6xl font-bold">
+              A Better way to track your job application
+            </h1>
+            <p className="text-muted-foreground mb-10 text-xl">
+              Capture, Organize, and manage your job search in one place
+            </p>
+            <div className="flex flex-col items-center gap-4 ">
+              <Link href="/sign-up">
+              <Button size="lg" className="h-12 px-8 text-lg font-medium">
+                Start for free <ArrowRight className="ml-2" />
+              </Button>
+              </Link>
+              <p className="text-sm text-muted-foreground">
+                Free forever. No credit card required.
+              </p>
+            </div>
           </div>
         </section>
       </main>
